fe71a9bdfaa6a90b37211854fe354990e498c35e
juanJvargas <your_email_address@example.com>
2019-08-22 10:29:18 -0500

    Probando nueas configuracion para el deploy

--- HuronShop/settings.py
+++ HuronShop/settings.py
@@ -132,14 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"), 
-    )
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
