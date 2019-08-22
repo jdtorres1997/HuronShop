8d1b287ae43410a3e4a0a19049af27909a2ab9bb
juanJvargas <your_email_address@example.com>
2019-08-22 11:52:54 -0500

    locals

--- HuronShop/settings.py
+++ HuronShop/settings.py
@@ -144,3 +144,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+django_heroku.settings(locals())
