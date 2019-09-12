7fd52f6802b219f0ecfd06b430eb90ad737101e7
juanJvargas <your_email_address@example.com>
2019-09-11 22:59:30 -0500

    catalogo

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,7 +6,7 @@ from apps.accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(redirect_authenticated_user=True,
+    path('', auth_views.LoginView.as_view(redirect_authenticated_user=True,
                     template_name='accounts/login.html'), name='login'),
     path('home', home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
