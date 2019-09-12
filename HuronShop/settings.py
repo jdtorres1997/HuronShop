dc4de08864edc46779c4ea82a0429b800aa1d3f1
juanJvargas <your_email_address@example.com>
2019-09-12 03:14:22 -0500

    settings

--- HuronShop/settings.py
+++ HuronShop/settings.py
@@ -136,7 +136,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 
 #LOCALLLLL
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 #https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
 
